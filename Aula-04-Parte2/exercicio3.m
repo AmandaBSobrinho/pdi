978b04f4954dd6d80b73e9c73b15c2d0b37a2941
Amanda Barbosa Sobrinho <amanda.barbosa@unesp.br>
2018-05-13 18:07:44 -0300

    Exercício 3 corrigido (equaliza só a camada I)

--- Aula-04-Parte2/exercicio3.m
+++ Aula-04-Parte2/exercicio3.m
@@ -1,6 +1,7 @@
 arq=input('Nome do arquivo: ','s');
 ext=input('Extensao do arquivo (jpg, jpeg, png): ','s');
 imagem=imread(arq,ext);
+info=imfinfo(strcat(arq,'.',ext));
 
 figure,imshow(imagem);
 title('Imagem Original');
@@ -14,17 +15,18 @@ imgHSI = rgbParahsi(red,green,blue);
 imgHSI = uint8(imgHSI);
 figure,imshow(imgHSI);
 title('Imagem HSI');
+%imsave; % Para salvar a imagem (teste)
 
 chH = imgHSI(:, :, 1);
 chS = imgHSI(:, :, 2);
 chI = imgHSI(:, :, 3);
 
-chHEq = equalizacao(chH,info);
-chSEq = equalizacao(chS,info);
+%chHEq = equalizacao(chH,info);
+%chSEq = equalizacao(chS,info);
 chIEq = equalizacao(chI,info);
 
 %Imagem final equalizada
-saida = cat(3, chHEq, chSEq, chIEq);
+saida = cat(3, chH, chS, chIEq);
 figure,imshow(saida);
 title('Imagem Equalizada Final');
 
