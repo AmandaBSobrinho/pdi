ebb1325903b39320bc2b3507c755d26720aba909
Bruno Leal <brunoleal@sjrp.unesp.br>
2018-05-15 01:50:00 -0300

    remoção de comentários em huffman_decode

--- Implementacao-Artigo/huffman_decode.m
+++ Implementacao-Artigo/huffman_decode.m
@@ -1,12 +1,5 @@
 function decompress = huffman_decode(compress, dict)
 
     decompress = huffmandeco(compress,dict);% decode the image.
-    
-    
-    %zeroindx=find(image==0);%find boundries
-    %inew=imagenew(1:zeroindx(1)-1); %seperate row index
-    %jnew=imagenew(zeroindx(1)+1:zeroindx(2)-1); %seperate column index
-    %blksznew=imagenew(zeroindx(2)+1:zeroindx(3)-1);%seperate blocksize
-    %avgnew=imagenew(zeroindx(3)+1:end); %seperate mean values
 
 end
