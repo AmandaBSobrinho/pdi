cb3f92e4a83060e45765391e3cdba0cda1611309
box3x4 <bruno.carvalho1505@hotmail.com>
2018-06-21 05:39:57 -0300

    fim

--- trab_brunos/matriz_confusao.m
+++ trab_brunos/matriz_confusao.m
@@ -1,13 +1,16 @@
-function [matriz_confusao] = matriz_confusao(matriz_carac)
+function [matriz_confusao, matriz_conf_final] = matriz_confusao(matriz_carac)
 
     [m, n] = size(matriz_carac);
     
     matriz_confusao = zeros(m,4,3);
+    matriz_conf_final = zeros(2,7,3);
     
     for j=1:n-1
+        maior = 0;
         for i=1:m
             tabela_ouro = zeros(2, 2);
             corte = matriz_carac(i,j);
+            disp(corte);
             classe = matriz_carac(i,4);
             for k=1:m
                 if matriz_carac(k,j) <= corte && classe == matriz_carac(k,4)
@@ -19,7 +22,7 @@ function [matriz_confusao] = matriz_confusao(matriz_carac)
                 elseif matriz_carac(k,j) <= corte && classe ~= matriz_carac(k,4)
                         
                     tabela_ouro(1,2) = tabela_ouro(1,2) + 1;
-                else
+                elseif matriz_carac(k,j) > corte && classe ~=matriz_carac(k,4)
                         
                     tabela_ouro(2,2) = tabela_ouro(2,2) + 1;
                 end
@@ -27,10 +30,23 @@ function [matriz_confusao] = matriz_confusao(matriz_carac)
             
             total = tabela_ouro(1,1)+tabela_ouro(1,2)+tabela_ouro(2,1)+tabela_ouro(2,2);
             
-            matriz_confusao(i,2,j) = 100 - (tabela_ouro(2,2) / (tabela_ouro(1,2) + tabela_ouro(2,2)))*100;
+            matriz_confusao(i,2,j) = (1 - (tabela_ouro(2,2) / (tabela_ouro(1,2) + tabela_ouro(2,2)))) * 100;
             matriz_confusao(i,1,j) = (tabela_ouro(1,1) / (tabela_ouro(1,1) + tabela_ouro(2,1))) * 100;
             matriz_confusao(i,3,j) = corte;
-            matriz_confusao(i,4,j) = (tabela_ouro(1,1)+tabela_ouro(2,2))/total;
+            matriz_confusao(i,4,j) = ((tabela_ouro(1,1)+tabela_ouro(2,2))/total) * 100;
+            
+            if matriz_confusao(i,4,j) > maior
+                maior = matriz_confusao(i,4,j);
+                matriz_conf_final(1,1,j) = tabela_ouro(1,1);
+                matriz_conf_final(1,2,j) = tabela_ouro(1,2);
+                matriz_conf_final(2,1,j) = tabela_ouro(2,1);
+                matriz_conf_final(2,2,j) = tabela_ouro(2,2);
+                matriz_conf_final(1,3,j) = matriz_confusao(i,1,j);
+                matriz_conf_final(1,4,j) = (tabela_ouro(2,2) / (tabela_ouro(1,2) + tabela_ouro(2,2)))*100;
+                matriz_conf_final(1,5,j) = matriz_confusao(i,4,j);
+                matriz_conf_final(1,6,j) = (tabela_ouro(1,1) /(tabela_ouro(1,1)+tabela_ouro(1,2))) * 100;
+                matriz_conf_final(1,7,j) = corte;
+            end
         end
     end
     
